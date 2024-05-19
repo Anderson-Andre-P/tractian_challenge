b301d6c1a1e9d03c2980dd3c51e5ff58cc31d359
Anderson-Andre-P <eleu.ander@gmail.com>
2024-05-19 15:13:05 -0300

    feat: modify the controller of base screen

--- lib/app/modules/base/controllers/base_controller.dart
+++ lib/app/modules/base/controllers/base_controller.dart
@@ -1,12 +1,19 @@
 import 'package:get/get.dart';
 
+import '../../home/controllers/home_controller.dart';
+
 class BaseController extends GetxController {
   // current screen index
   int currentIndex = 0;
 
+  final HomeController homeController = Get.put(HomeController());
+
   /// change the selected screen index
-  changeScreen(int selectedIndex) {
+  void changeScreen(int selectedIndex) {
     currentIndex = selectedIndex;
+    if (currentIndex == 0) {
+      homeController.getData();
+    }
     update();
   }
 }
