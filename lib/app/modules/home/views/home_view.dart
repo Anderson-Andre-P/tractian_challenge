dc07c8081ded1920a88bf13ecd9ac4785a2f8323
Anderson-Andre-P <eleu.ander@gmail.com>
2024-05-19 15:14:06 -0300

    feat: change the layout and functions of home screen

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
+import '../../../components/api_error_widget.dart';
+import '../../../components/my_widget_animator.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,12 +17,30 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: GetBuilder<HomeController>(builder: (_) {
+        return MyWidgetsAnimator(
+          apiCallStatus: controller.apiCallStatus,
+          loadingWidget: () => const Center(
+            child: CupertinoActivityIndicator(),
+          ),
+          errorWidget: () => ApiErrorWidget(
+            message: "Internet Error",
+            retryAction: () => controller.getData(),
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+          ),
+          successWidget: () => SingleChildScrollView(
+            child: Column(
+              children: [
+                Center(
+                  child: Text(
+                    controller.data![0]['id'].toString(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
