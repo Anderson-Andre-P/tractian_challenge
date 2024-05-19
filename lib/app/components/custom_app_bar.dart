553aa259c220eddb1e448a704aacf6fa7bc85526
Anderson-Andre-P <eleu.ander@gmail.com>
2024-05-19 17:59:25 -0300

    feat: new component

---
+++ lib/app/components/custom_app_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSize {
+  final String title;
+  final bool hasDrawer;
+  final List<IconData>? actionIcons;
+  final List<VoidCallback>? onIconPressed;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.actionIcons,
+    this.onIconPressed,
+    this.hasDrawer = false,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(52.0);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.theme;
+    return AppBar(
+      title: Text(
+        title,
+        style: theme.textTheme.displayMedium?.copyWith(
+          fontSize: 23.sp,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+      leading: hasDrawer
+          ? IconButton(
+              icon: const Icon(Icons.arrow_back_outlined),
+              color: theme.iconTheme.color,
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          : null,
+      centerTitle: true,
+      backgroundColor: theme.primaryColor,
+      actions: _buildActionIcons(),
+    );
+  }
+
+  List<Widget> _buildActionIcons() {
+    List<Widget> widgets = [];
+
+    if (actionIcons != null && onIconPressed != null) {
+      for (int i = 0; i < actionIcons!.length; i++) {
+        widgets.add(
+          IconButton(
+            icon: Icon(actionIcons![i]),
+            onPressed: onIconPressed![i],
+            color: Get.theme.iconTheme.color,
+          ),
+        );
+      }
+    }
+
+    return widgets;
+  }
+
+  @override
+  Widget get child => throw UnimplementedError();
+}
