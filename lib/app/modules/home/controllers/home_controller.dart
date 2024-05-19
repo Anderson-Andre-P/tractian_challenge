4a17407d8225f07a4dbe8fbe861689c569caf3e9
Anderson-Andre-P <eleu.ander@gmail.com>
2024-05-19 15:13:40 -0300

    feat: controller of home screen

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,37 @@
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {
-  //TODO: Implement HomeController
+import '../../../../utils/constants.dart';
+import '../../../services/api_call_status.dart';
+import '../../../services/base_client.dart';
 
-  final count = 0.obs;
+class HomeController extends GetxController {
+  List<dynamic>? data;
+//   Map<String, dynamic>? data;
+  ApiCallStatus apiCallStatus = ApiCallStatus.holding;
+
+  getData() async {
+    apiCallStatus = ApiCallStatus.loading;
+    update();
+    await BaseClient.safeApiCall(
+      Constants.companiesApiUrl(),
+      RequestType.get,
+      onSuccess: (response) {
+        data = List.from(response.data);
+        // data = response.data;
+        apiCallStatus = ApiCallStatus.success;
+        update();
+      },
+      onError: (error) {
+        BaseClient.handleApiError(error);
+        apiCallStatus = ApiCallStatus.error;
+        update();
+      },
+    );
+  }
+
   @override
   void onInit() {
+    getData();
     super.onInit();
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
