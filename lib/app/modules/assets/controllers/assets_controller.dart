f780f62421062d9b83dcef4aa1bdc21ad83c8488
Anderson-Andre-P <eleu.ander@gmail.com>
2024-05-19 20:56:37 -0300

    feat: show the locations and components

--- lib/app/modules/assets/controllers/assets_controller.dart
+++ lib/app/modules/assets/controllers/assets_controller.dart
@@ -9,7 +9,7 @@ class AssetsController extends GetxController {
 
   AssetsController(this.companyId);
 
-  var assets = [].obs;
+  var assets = <Assets>[].obs; // Alteração de RxList para List
 
   @override
   void onInit() {
@@ -22,11 +22,10 @@ class AssetsController extends GetxController {
       final response =
           await http.get(Uri.parse(Constants.assetsApiUrl(companyId)));
       if (response.statusCode == 200) {
-        final List<dynamic> responseData = json.decode(response.body);
+        final List<dynamic> responseData =
+            json.decode(response.body) as List<dynamic>;
         if (responseData.isNotEmpty) {
-          assets.value = responseData;
-        } else {
-          print('No assets found for company $companyId');
+          assets.value = responseData.map((e) => Assets.fromJson(e)).toList();
         }
       } else {
         print('Failed to load assets: ${response.statusCode}');
@@ -34,5 +33,40 @@ class AssetsController extends GetxController {
     } catch (e) {
       print('Error loading assets: $e');
     }
+  }
+}
+
+class Assets {
+  String? gatewayId;
+  String? id;
+  String? locationId;
+  String? name;
+  String? parentId;
+  String? sensorId;
+  String? sensorType;
+  String? status;
+
+  Assets({
+    this.gatewayId,
+    this.id,
+    this.locationId,
+    this.name,
+    this.parentId,
+    this.sensorId,
+    this.sensorType,
+    this.status,
+  });
+
+  factory Assets.fromJson(Map<String, dynamic> json) {
+    return Assets(
+      gatewayId: json['gatewayId'],
+      id: json['id'],
+      locationId: json['locationId'],
+      name: json['name'],
+      parentId: json['parentId'],
+      sensorId: json['sensorId'],
+      sensorType: json['sensorType'],
+      status: json['status'],
+    );
   }
 }
--- lib/app/modules/assets/views/assets_view.dart
+++ lib/app/modules/assets/views/assets_view.dart
@@ -3,34 +3,83 @@ import 'package:get/get.dart';
 
 import '../controllers/assets_controller.dart';
 
-class AssetsView extends GetView<AssetsController> {
+class AssetsView extends StatelessWidget {
   const AssetsView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final AssetsController controller = Get.find<AssetsController>();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AssetsView'),
+        title: const Text('Assets View'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Obx(() {
-          final assets = controller.assets;
+      body: Obx(() {
+        if (controller.assets.isEmpty) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          return TreeView(locations: controller.assets);
+        }
+      }),
+    );
+  }
+}
 
-          if (assets.isEmpty) {
-            return const Text(
-              'No assets available',
-              style: TextStyle(fontSize: 20),
-            );
-          } else {
-            final firstAssetName = assets[0]['name'].toString();
-            return Text(
-              'First Asset Name: $firstAssetName',
-              style: const TextStyle(fontSize: 20),
-            );
-          }
-        }),
-      ),
+class TreeView extends StatelessWidget {
+  final List<Assets> locations;
+  final double nodeIndent;
+
+  const TreeView({
+    super.key,
+    required this.locations,
+    this.nodeIndent = 16.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: locations
+          .where((loc) => loc.parentId == null)
+          .map((location) => buildNode(location, locations, 0.0))
+          .toList(),
+    );
+  }
+
+  Widget buildNode(
+      Assets location, List<Assets> allLocations, double marginLeft) {
+    List<Widget> children = [];
+
+    List<Assets> directChildren =
+        allLocations.where((loc) => loc.parentId == location.id).toList();
+
+    for (var child in directChildren) {
+      children.add(buildNode(child, allLocations, marginLeft + nodeIndent));
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: marginLeft),
+          child: ListTile(
+            title: Text(location.name ?? 'No Name'),
+            subtitle: Text(location.locationId != null
+                ? '[Location]'
+                : location.sensorType != null
+                    ? '[Component - ${location.sensorType}]'
+                    : '[Asset]'),
+          ),
+        ),
+        if (children.isNotEmpty)
+          Padding(
+            padding: EdgeInsets.only(left: marginLeft + nodeIndent),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: children,
+            ),
+          ),
+      ],
     );
   }
 }
