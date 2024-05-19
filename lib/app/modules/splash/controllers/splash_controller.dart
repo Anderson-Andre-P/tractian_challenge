bdf7ad837e6020c0a8d37ef8ce75fa2d6e38077b
Anderson-Andre-P <eleu.ander@gmail.com>
2024-05-19 15:12:38 -0300

    refactor: change time of splash

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -5,7 +5,7 @@ import '../../../routes/app_pages.dart';
 class SplashController extends GetxController {
   @override
   void onInit() async {
-    await Future.delayed(const Duration(seconds: 10));
+    await Future.delayed(const Duration(seconds: 1));
     Get.offNamed(Routes.ONBOARDING);
     super.onInit();
   }
