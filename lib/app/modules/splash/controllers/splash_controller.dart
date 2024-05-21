facbe4650b918dda0d080e9af2de5df8d3db4f67
Anderson-Andre-P <eleu.ander@gmail.com>
2024-05-21 13:01:50 -0300

    refactor: change yime in delay

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -5,8 +5,8 @@ import '../../../routes/app_pages.dart';
 class SplashController extends GetxController {
   @override
   void onInit() async {
-    await Future.delayed(const Duration(seconds: 1));
-    Get.offNamed(Routes.ONBOARDING);
+    await Future.delayed(const Duration(seconds: 2));
+    Get.offNamed(Routes.HOME);
     super.onInit();
   }
 }
