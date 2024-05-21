1d15298e4df765a20f0008fbd721747a84e24cc2
Anderson-Andre-P <eleu.ander@gmail.com>
2024-05-21 13:12:49 -0300

    fix: redirect

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -6,7 +6,7 @@ class SplashController extends GetxController {
   @override
   void onInit() async {
     await Future.delayed(const Duration(seconds: 2));
-    Get.offNamed(Routes.HOME);
+    Get.offNamed(Routes.BASE);
     super.onInit();
   }
 }
